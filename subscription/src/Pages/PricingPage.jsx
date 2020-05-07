830003c9318c8a5a0e7ee94b47a3ef92abccc4f2
JorPoon <jormpoon@gmail.com>
2020-05-07 14:46:26 -0400

    created navbar component and installed material ui dependency

--- subscription/src/Pages/PricingPage.jsx
+++ subscription/src/Pages/PricingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PricingView from '../views/PricingView'
+import Navbar from '../components/heading/NavBar'
 
 export const PricingPage = () => {
     return (
         <>
+        <NavBar/>
         <PricingView/>
         </>
     )
