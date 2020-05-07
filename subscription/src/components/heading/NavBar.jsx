a083fc16a47b82cecf4d0ed30985b7e38cce29eb
JorPoon <jormpoon@gmail.com>
2020-05-07 14:55:46 -0400

    creating pricing card component and adding to pricing view component

--- subscription/src/components/heading/NavBar.jsx
+++ subscription/src/components/heading/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const NavBar = () => {
     return (
         <div className="NavBar">
-
+            <h1>PlaceHolder Heading</h1>
         </div>
     )
 }
--- subscription/src/views/PricingView.jsx
+++ subscription/src/views/PricingView.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import PricingCard from '../components/pricing/PricingCard'
 
 
 export const PricingView = () => {
     return (
         <div className='Pricing-View'>
-
+            <PricingCard/>
+            <PricingCard/>
+            <PricingCard/>
         </div>
     )
 }
