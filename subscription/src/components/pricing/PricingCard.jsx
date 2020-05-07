fe95394f758b4509df1d5cb9a898787c460e8ba7
JorPoon <jormpoon@gmail.com>
2020-05-07 15:01:00 -0400

    adding material ui components to pricingcard

--- subscription/src/components/pricing/PricingCard.jsx
+++ subscription/src/components/pricing/PricingCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 
 export const PricingCard = (props) => {
     return (
-        
+        <>
+            <Card>
+                <CardHeader>Title</CardHeader>
+                <CardContent>Content</CardContent>
+                <Button>Subscribe!</Button>
+            </Card>
+        </>
     )
 }
